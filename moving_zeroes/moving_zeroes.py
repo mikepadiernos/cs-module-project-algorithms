a0a72379ebb8d3e51bc4a45893b19454b22d6b00
mikepadiernos <mike.padiernos@gmail.com>
2020-08-11 23:07:23 -0400

    [FEATURE] completion / moving_zeroes.py
    - passed moving_zeroes.py test

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,15 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    moved = 0
+    for n in range(0, len(arr)):
+        if arr[n] is not 0:
+            arr[moved], arr[n] = arr[n], arr[moved]
+            moved += 1
+    return arr
 
 
 if __name__ == '__main__':
